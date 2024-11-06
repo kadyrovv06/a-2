3a18570d48509e75c74d6e7cbd9231fb4849f85f
Gege <kadyrovbaiel00@gmail.com>
2024-11-06 16:59:51 +0600

    first

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+var API_PRODUCTS = "https://dummyjson.com/products"
+
 var box = document.querySelector(".box")
+
+async function getProducts() {
+    try {
+        const res = await fetch(API_PRODUCTS)
+        const data = await res.json()
+        console.log(data);
+        showProduct(data.products)
+    } catch (error) {
+        console.log(error);
+    }
+}
+getProducts()
+
+function showProduct(products) {
+    box.innerHTML = products.map((item) => {
+
+        return ` <div>
+            <p>${item.title}</p>
+            <img src="${item.thumbnail}"/>
+        </div> `
+    })
+}
